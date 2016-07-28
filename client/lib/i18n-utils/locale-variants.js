0e90cb3059609a040a679bbbb4002550fe526411
Yoav Farhi <yoav@farhi.org>
2016-07-28 12:14:52 +0300

    Add new translations for formal german

--- client/lib/i18n-utils/locale-variants.js
+++ client/lib/i18n-utils/locale-variants.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import debugFactory from 'debug';
+import request from 'superagent';
 import to_latin from 'cyrillic-to-latin';
 import i18n from 'i18n-calypso';
 
@@ -9,23 +10,31 @@ import i18n from 'i18n-calypso';
  * Internal dependencies
  */
 import userSettings from 'lib/user-settings';
+import i18nUtils from 'lib/i18n-utils';
 
 const debug = debugFactory( 'calypso:i18n' );
 
 const localeVariants = {
 	init() {
-		const locale_variant = userSettings.getSetting( 'locale_variant' );
-		switch ( locale_variant ) {
+		const localeVariant = userSettings.getSetting( 'locale_variant' );
+		switch ( localeVariant ) {
+			case 'de_formal':
+				debug( 'Adding new translations for ' + localeVariant );
+				this.addTranslations( localeVariant );
+
+				break;
+
 			case 'sr_latin':
+				debug( 'Applying mods for ' + localeVariant );
 				i18n.registerTranslateHook( ( translation ) => {
-					return this.cyrillic_to_latin( translation );
+					return this.cyrillicToLatin( translation );
 				} );
 				i18n.reRenderTranslations();
 				break;
 		}
 	},
 
-	cyrillic_to_latin( translation ) {
+	cyrillicToLatin( translation ) {
 		switch ( typeof( translation ) ) {
 			case 'object':
 				for ( let prop in translation ) {
@@ -40,6 +49,16 @@ const localeVariants = {
 		}
 
 		return translation;
+	},
+
+	addTranslations( localeVariantSlug ) {
+		request.get( i18nUtils.languageFileUrl( localeVariantSlug ) ).end( function( error, response ) {
+			if ( error ) {
+				debug( 'Encountered an error loading locale file for ' + localeVariantSlug );
+				return;
+			}
+			i18n.addTranslations( response.body );
+		} );
 	}
 };
 
